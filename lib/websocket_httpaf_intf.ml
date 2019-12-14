3308726f81dbf204657c225b29e88c642b0706d2
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-12-14 21:24:27 +0000

    Base compiler >= 4.08 (#1)
    * updated dependencies including the compiler (>= 4.08)
    
    * downgraded the versions of some dependencies to improve version solver perf
    
    * corrected an obsolete installation requirement

--- lib/websocket_httpaf_intf.ml
+++ lib/websocket_httpaf_intf.ml
@@ -5,7 +5,7 @@ module type Websocket_httpaf = sig
   include module type of struct include Websocket end
 
   val upgrade_connection : ?headers:Httpaf.Headers.t ->
-                           'handle Httpaf.Reqd.t ->
+                           Httpaf.Reqd.t ->
                            file_descr ->
                            (Websocket.Frame.t -> unit) ->
                            (Httpaf.Response.t * [ `write ] Httpaf.Body.t *
